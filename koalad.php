422a040f89142c2b0b5dd145dccdedbdd4f8e682
Clay Freeman <git@clayfreeman.com>
2015-03-28 01:10:08 -0500

    Close sockets when finished with requests.

--- koalad.php
+++ koalad.php
@@ -1,34 +1,39 @@
 <?php
   require_once(dirname(__FILE__)."/koalasignd.php");
   class koalad {
-    private $configured = false;
+    private $host = null;
     private $socket = null;
 
     public function __construct($host) {
-      $this->socket = fsockopen("tls://".$host, "3654");
-      if (is_resource($this->socket))
-        $this->configured = true;
-      return $this->configured;
+      $this->host = "tls://".$host;
+    }
+
+    private function connect() {
+      $sock = fsockopen($this->host, "3654");
+      if (is_resource($sock)) {
+        $this->socket = $sock;
+        return true;
+      }
+      return false;
     }
 
     public function sendCommand($c, $d) {
-      if ($this->configured == true) {
-        if (is_string($c) && $c != null) {
-          $payload = json_encode(array(
-            "command" => $c,
-            "data"    => $d
-          ));
-          $request = koalasignd::getRequest($payload);
-          if (json_decode($request) != false) {
-            fputs($this->socket, $request."\n");
-            $time = time();
-            $data = null;
-            while (!feof($this->socket) && (time() - $time) < 5)
-              $data .= fgets($this->socket);
-            $data = json_decode($data, true);
-            if (is_array($data))
-              return $data;
-          }
+      if (is_string($c) && $c != null) {
+        $payload = json_encode(array(
+          "command" => $c,
+          "data"    => $d
+        ));
+        $request = koalasignd::getRequest($payload);
+        if (json_decode($request) != false && $this->connect() == true) {
+          fputs($this->socket, $request."\n");
+          $time = time();
+          $data = null;
+          while (!feof($this->socket) && (time() - $time) < 5)
+            $data .= fgets($this->socket);
+          fclose($this->socket);
+          $data = json_decode($data, true);
+          if (is_array($data))
+            return $data;
         }
       }
       return false;
--- koalasignd.php
+++ koalasignd.php
@@ -3,20 +3,22 @@
     private static $socket = null;
 
     private static function connect() {
-      self::$socket = fsockopen("tls://127.0.0.1", "4765");
-      if (is_resource(self::$socket)) {
+      $sock = fsockopen("tls://127.0.0.1", "4765");
+      if (is_resource($sock)) {
+        self::$socket = $sock;
         return true;
       }
       return false;
     }
 
     public static function getRequest($payload) {
-      if (self::connect() && $payload != null) {
+      if ($payload != null && self::connect() == true) {
         fputs(self::$socket, $payload."\n");
         $time = time();
         $data = null;
         while (!feof(self::$socket) && (time() - $time) < 5)
           $data .= fgets(self::$socket);
+        fclose(self::$socket);
         $data = json_decode($data, true);
         if (is_array($data) && isset($data["status"]) && isset($data["data"]) &&
             $data["status"] == "200") {
